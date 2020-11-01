202a4f6b10f26f907f42a3bf9d3da4d8dbffc056
Jaishree Pandey <jaishreepandey@Jaishrees-MacBook-Air.local>
2020-11-01 18:46:17 +0530

    task 2

--- basics/check_palindrome.js
+++ basics/check_palindrome.js
@@ -1,11 +1,7 @@
-var x = "MOMA";
-var y = 0;
 
-console.log("Print the original string",x);
-
-console.log(checkPalindrome(x));
 
 function checkPalindrome(x){
+	console.log("Print the original string",x);
 	for (var i = 0, j = x.length-1; i < x.length, j >= 0; i++, j--) {
 		 if (x[i] != x[j]) 
 		 	return false;
